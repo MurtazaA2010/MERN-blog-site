1db8180449dc1cd17fd290bc2899fa365988b7ce
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-07 14:46:33 +0600

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {BrowserRouter as Router, Switch , Route } from 'react-router-dom'
+import Header from './Components/Header';
+function App() {
+  return (
+   
+      <Router>
+        <Header />
+      </Router>
+
+  );
+}
+
+export default App;
---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,19 @@
+
+const Header = () => {
+    return ( 
+      
+            <div className="Header">
+                <header>
+                    <h3>MERN BLOG APP</h3>
+                </header>
+                <nav>
+                    <a href="/signup">Sign Up</a>
+                    <a href="/login">Login</a>
+                </nav>
+            </div>
+       
+
+     );
+}
+ 
+export default Header;
