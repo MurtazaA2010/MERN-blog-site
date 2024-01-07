2e3f554733368f8fcab445d1311659cf7af7c177
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-07 20:48:14 +0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import {BrowserRouter as Router, Switch , Route } from 'react-router-dom'
 import Header from './Components/Header';
+import Blog from './Components/Blog';
 function App() {
   return (
    
       <Router>
         <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Blog />
+          </Route>
+        </Switch>
       </Router>
 
   );
