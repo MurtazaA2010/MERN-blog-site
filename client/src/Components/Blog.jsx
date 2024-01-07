528c7829c12e88abd546004c5687dfe3ff9dc940
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-07 20:44:09 +0600

    Add files via upload

---
+++ client/src/Components/Blog.jsx
@@ -0,0 +1,48 @@
+import '../blog.css'
+const Blog = () => {
+    return ( 
+        <div className="blogs">
+            <div className="blog-each">
+                <div className="blog-img">
+                    <img src="https://abctrainings.in/media/thumbnails/Python-01_2_1.png" alt="" />
+                </div>
+                <div className="blog-txt">
+                    <div className="blog-title">
+                        <h3>Python for Web Development</h3>
+                    </div>
+                    <div className="blog-author">
+                        <p>Author : Murtaza</p>
+                    </div>
+                    <div className="blog-time">
+                        <p>2023-1-1 6:00pm</p>
+                    </div>
+                    <div className="blog-details">
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem libero et eveniet amet obcaecati pariatur voluptate dignissimos! Iure, sunt omnis.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="blog-each">
+                <div className="blog-img">
+                    <img src="https://abctrainings.in/media/thumbnails/Python-01_2_1.png" alt="" />
+                </div>
+                <div className="blog-txt">
+                    <div className="blog-title">
+                        <h3>Python for Web Development</h3>
+                    </div>
+                    <div className="blog-author">
+                        <p>Author : Murtaza</p>
+                    </div>
+                    <div className="blog-time">
+                        <p>2023-1-1 6:00pm</p>
+                    </div>
+                    <div className="blog-details">
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem libero et eveniet amet obcaecati pariatur voluptate dignissimos! Iure, sunt omnis.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+     );
+}
+ 
+export default Blog;
