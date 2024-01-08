0bd9f01962fb74662fcdc320d4d458acbdc29ec4
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-08 10:35:17 +0600

    Update signin.jsx

--- client/src/Components/signin.jsx
+++ client/src/Components/signin.jsx
@@ -1,9 +1,19 @@
-const Signin = () => {
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+const SignUp = () => {
     return ( 
-        <div className="Signin">
+        <div className="sign-in">
             <h3>Sign In</h3>
+            <form action="">
+                <input type="email" placeholder="Enter you email" required/>
+                <br />
+                <input type="password" placeholder="Enter you Password" required />
+                <br />
+                <button>Sign In</button>
+            </form>
+            <p>Don't have an account? <Link to='/signup'>Sign Up</Link></p>
         </div>
+
      );
 }
  
-export default Signin;
+export default SignUp;
