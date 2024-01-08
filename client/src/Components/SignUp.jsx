4971f403c9b5cb7cdde40155ad224510153b1d43
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-08 10:34:57 +0600

    Update SignUp.jsx

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const SignUp = () => {
     return ( 
-        <div className="signup">
+        <div className="sign-up">
             <h3>Sign Up</h3>
+            <form action="">
+                <input type="email" placeholder="Enter you email" required/>
+                <br />
+                <input type="password" placeholder="Enter you Password" required />
+                <br />
+                <button>Sign Up</button>
+            </form>
+            <p>Already have an account? <Link to='/login'>Sign In</Link></p>
+
         </div>
+
      );
 }
  
