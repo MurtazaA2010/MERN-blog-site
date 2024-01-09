06b6df464bd3f715b1d3e1027e959fdb7bdb5960
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-09 22:29:24 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const cors = require('cors');
 
+
+const app = express();
+
+app.listen(3001);
+
+app.use(cors());
+app.use(express.json())
+
+app.post('/request', (req, res)=> {
+    const {username, pass} = req.body;
+    res.json({requstData: {username, pass}})
+
+})
