dece47868e16b623d5f1372383458d7ba2b9890b
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-10 10:31:41 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-
+const { default: mongoose } = require('mongoose');
+const User = require('./models/User')
 
 const app = express();
 
-app.listen(3001);
+mongoose.connect('mongodb://127.0.0.1:27017/BLOGAPP').then(()=> {
+    console.log('Connected to mongo DB')
+    app.listen(3001);
+}).catch(err => {
+    console.log(err);
+})
+
+
 
 app.use(cors());
 app.use(express.json())
 
 app.post('/request', (req, res)=> {
-    const {username, pass} = req.body;
-    res.json({requstData: {username, pass}})
-
+    const {username, password} = req.body;
+    const user = new User ({username, password});
+    user.save().then((result)=> {
+        console.log(result);
+    }).catch(err => {
+        res.status(404).res.json('failed')
+    });
 })
