0b108db3bb6bea39ea36eb033ee836763d956339
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-11 18:58:15 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,7 +11,8 @@ const app = express();
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/BLOGAPP').then(()=> {
-    console.log('Connected to mongo DB')
+    console.log('Connected to mongo DB');
+
     app.listen(3001);
 }).catch(err => {
     console.log(err);
@@ -22,7 +23,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/BLOGAPP').then(()=> {
 app.use(cors());
 app.use(express.json())
 
-app.post('/request', (req, res)=> {
+app.post('/signup', (req, res)=> {
     const {username, password} = req.body;
     const user = new User ({
         username, 
@@ -33,4 +34,11 @@ app.post('/request', (req, res)=> {
     }).catch(err => {
         res.status(404)
     });
+});
+
+app.post('/signin', async (req, res)=> {
+    const {username, password} = req.body;
+    const user = await User.findOne({username});
+    
+    
 })
