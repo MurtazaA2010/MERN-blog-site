00b736ecf35beefd1be9a968629ddb03403f849a
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-11 18:41:14 +0600

    Update signin.jsx

--- client/src/Components/signin.jsx
+++ client/src/Components/signin.jsx
@@ -1,12 +1,27 @@
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
-const SignUp = () => {
+import { useState } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+const Signin= () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
     return ( 
         <div className="sign-in">
             <h3>Sign In</h3>
             <form action="">
-                <input type="email" placeholder="Enter you email" required/>
+                <input 
+                type="text" 
+                placeholder="Username" 
+                required
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                />
                 <br />
-                <input type="password" placeholder="Enter you Password" required />
+                <input
+                 type="password" 
+                 placeholder="Enter you Password" 
+                 required
+                 value={password}
+                 onChange={e => setPassword(e.target.value)}
+                  />
                 <br />
                 <button>Sign In</button>
             </form>
@@ -16,4 +31,4 @@ const SignUp = () => {
      );
 }
  
-export default SignUp;
+export default Signin;
