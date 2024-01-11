81d1ee40f857fc9d068cc9b811e6695d3e82667b
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-11 21:09:38 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,23 +4,24 @@ const { default: mongoose } = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
 
 const app = express();
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/BLOGAPP').then(()=> {
     console.log('Connected to mongo DB');
+    app.listen(4000)
 
-    app.listen(3001);
 }).catch(err => {
     console.log(err);
 })
 
 
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    credentials: true,
+}));
 app.use(express.json())
 
 app.post('/signup', (req, res)=> {
@@ -29,16 +30,25 @@ app.post('/signup', (req, res)=> {
         username, 
         password:bcrypt.hashSync(password, saltRounds)
     });
-    user.save().then((result)=> {
+    user.save()
+    .then((result) => {
         console.log(result);
-    }).catch(err => {
-        res.status(404)
+        res.status(200).json({ message: 'Registration successful' });
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).json({ message: 'Internal Server Error' });
     });
 });
 
 app.post('/signin', async (req, res)=> {
     const {username, password} = req.body;
     const user = await User.findOne({username});
-    
-    
-})
+
+    if (user) {
+        const passOk = bcrypt.compareSync(password, user.password);
+        res.json(passOk);
+    } else {
+        res.json(false); // User not found
+    }
+});
