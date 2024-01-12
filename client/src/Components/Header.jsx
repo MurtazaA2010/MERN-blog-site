2834b03b9d043300a08704c7d3ba1f04f4343944
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-12 21:37:20 +0600

    Update Header.jsx

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,5 +1,11 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 const Header = () => {
+    useEffect (()=> {
+        fetch('http://localhost:4000/profile', {
+            credentials: 'include',
+        })
+    }, [])
     return ( 
       
             <div className="Header">
@@ -8,7 +14,7 @@ const Header = () => {
                 </header>
                 <nav>
                     <Link className="signup" to="/signup">Sign Up</Link>
-                    <Link to="/signin">Sign In</Link>
+                    <Link to="/signin">Login</Link>
                 </nav>
             </div>
        
