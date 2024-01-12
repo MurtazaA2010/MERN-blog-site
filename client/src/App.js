6a8a8da0ccddcbe7ba041c02a85173597f7ee188
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-12 14:58:06 +0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,56 @@
-//imports
-import './App.css';
-import {BrowserRouter as Router, Switch , Route } from 'react-router-dom'
-import Header from './Components/Header';
-import Blog from './Components/Blog';
-import SignUp from './Components/SignUp';
-import Signin from './Components/Signin';
+const express = require('express');
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+const saltRounds = 10;
+const secret = 'hdskjfj49353kjdfsdjf';
+
+const app = express();
 
 
-function App() {
-  return (
-   
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path='/'>
-            <Blog />
-          </Route>
-          <Route path='/signup'>
-            <SignUp />
-          </Route>
-          <Route path='/login'>
-            <Signin />
-          </Route>
-        </Switch>
-      </Router>
+mongoose.connect('mongodb://127.0.0.1:27017/BLOGAPP').then(()=> {
+    console.log('Connected to mongo DB');
+    app.listen(4000)
 
-  );
-}
+}).catch(err => {
+    console.log(err);
+})
 
-export default App;
+
+
+app.use(cors());
+app.use(express.json())
+
+app.post('/signup', (req, res)=> {
+    const {username, password} = req.body;
+    const user = new User ({
+        username, 
+        password:bcrypt.hashSync(password, saltRounds)
+    });
+    user.save()
+    .then((result) => {
+        console.log(result);
+        res.status(200).json({ message: 'Registration successful' });
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).json({ message: 'Internal Server Error' });
+    });
+});
+
+app.post('/signin', async (req, res)=> {
+    const {username, password} = req.body;
+    const user = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, user.password);
+
+    if(passOk) {
+        jwt.sign({username,id: user._id}, secret).then ((err, token)=> {
+            if(err) throw err;
+            res.json(token);
+        })
+    } else {
+        res.status(400).json('wrong credential')
+    }
+});
