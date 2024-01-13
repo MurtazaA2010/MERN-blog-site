d595e1470c6998f7823da52c2ee1d92cc7659127
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-13 10:46:44 +0600

    Update Header.jsx

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,25 +1,59 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-const Header = () => {
-    useEffect (()=> {
-        fetch('http://localhost:4000/profile', {
-            credentials: 'include',
-        })
-    }, [])
-    return ( 
-      
-            <div className="Header">
-                <header>
-                    <h3><Link to="/">MERN BLOG APP</Link></h3>
-                </header>
-                <nav>
-                    <Link className="signup" to="/signup">Sign Up</Link>
-                    <Link to="/signin">Login</Link>
-                </nav>
-            </div>
-       
 
-     );
-}
- 
+const Header = () => {
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((userInfo) => {
+        setUsername(userInfo.username);
+      })
+      .catch((error) => {
+        console.error('Error fetching user profile:', error);
+        // Handle the error, e.g., redirect to login page or show an error message
+      });
+  }, []);
+
+  const logout = () => {
+
+  }
+
+  return (
+    <div className="Header">
+      <header>
+        <h3>
+          <Link to="/">MERN BLOG APP</Link>
+        </h3>
+      </header>
+      <nav>
+        {username ? (
+          <>
+            <Link className="signup" to="/create">
+            Create New Post
+            </Link>
+            <a onClick={logout}> Logout({username})</a>
+          </>
+          
+        ) : (
+          <>
+            <Link className="signup" to="/signup">
+              Sign Up
+            </Link>
+            <Link to="/signin">Login</Link>
+          </>
+        )}
+      </nav>
+    </div>
+  );
+};
+
 export default Header;
