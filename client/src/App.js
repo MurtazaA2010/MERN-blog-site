218193db9753ccfd7dd26dbb1c28da92b9528fe6
Murtaza Abdullah <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-15 14:43:32 +0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@
 import './App.css';
 import {BrowserRouter as Router, Switch , Route } from 'react-router-dom'
 import Header from './Components/Header';
-import Blog from './Components/Blog';;
+import Blog from './Components/Blog';
 import SignUp from './Components/SignUp';
 import Signin from './Components/Signin';
+import { UserContextProvider } from './UserContext';
+import Create from './Components/Create';
+
 
 function App() {
   return (
-      <Router>
+   <UserContextProvider>
+     <Router>
         <Header />
         <Switch>
           <Route exact path='/'>
@@ -20,8 +24,13 @@ function App() {
           <Route path='/signin'>
             <Signin />
           </Route>
+          <Route path='/create'>
+            <Create />
+          </Route>
         </Switch>
       </Router>
+
+   </UserContextProvider>
   );
 }
 
