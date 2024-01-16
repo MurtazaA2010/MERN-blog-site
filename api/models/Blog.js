d1fe51393dc6c34e8939dd8b188b8debc34056e8
Murtaza Abdullah <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-16 21:24:31 +0600

    Update Blog.js

--- api/models/Blog.js
+++ api/models/Blog.js
@@ -6,7 +6,25 @@ const BlogSchema = new Schema({
     title: {
         type: String,
         required : true
+    },
+    snippet: {
+        type: String,
+        required : true,
+    },
+    content : {
+        type: String,
+        required: true
+    },
+    file : {
+        type : String,
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref:'User'
     }
+}, {
+    timestamps : true
 })
 
 const Blog = mongoose.model('Blog', BlogSchema);
