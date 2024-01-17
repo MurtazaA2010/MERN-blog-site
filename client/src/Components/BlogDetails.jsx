793f6fbbed630da6d50c423d2550803d6276e213
Murtaza Abdullah <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-17 21:39:10 +0600

    Update BlogDetails.jsx

--- client/src/Components/BlogDetails.jsx
+++ client/src/Components/BlogDetails.jsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useContext, useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom/cjs/react-router-dom.min";
+import '../CSS/details.css';
+import {format} from'date-fns';
+import { UserContext } from "../UserContext";
 
 const BlogDetails = () => {
     const {id} = useParams();
     const [blogInfo, setBlogInfo] = useState(null);
+    const {userInfo} = useContext(UserContext);
     useEffect(() => {
         fetch(`http://localhost:4000/blogs/${id}`)
             .then((response) => {
@@ -22,6 +26,19 @@ const BlogDetails = () => {
         <div className="blog-details">
             <div className="title">
                 <h1>{blogInfo.title}</h1>
+            </div>
+            <div className="date">
+                <time>First Published On {format(new Date(blogInfo.createdAt), 'MMM d, yyyy  h:mm:ss aa')}</time>
+                <br />
+                <time>Last Updated On {format(new Date(blogInfo.updatedAt), 'MMM d, yyyy h:mm aa')}</time>
+            </div>
+            <div className="edit">
+                {userInfo.id === blogInfo.author._id && (
+                    <button><Link to={`/edit/${blogInfo._id}`}>Edit This Blog</Link></button>
+                )} 
+            </div>
+            <div className="author">
+                <p>by @{blogInfo.author.username}</p>
             </div>
             <div className="image">
                 <img src={`http://localhost:4000/${blogInfo.file}`} alt="" />
