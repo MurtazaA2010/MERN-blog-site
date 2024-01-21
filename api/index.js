8c7ea7e573d6fa3f5c1af59f13b88e95262f1dfc
Murtaza Abdullah <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-21 22:32:27 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -28,6 +28,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(cookieparser());
+app.use(express.urlencoded({extended : true}))
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
 app.post('/signup', (req, res)=> {
@@ -74,6 +75,23 @@ app.post('/signin', async (req, res) => {
     }
 });
 
+
+app.post('/logout', (req, res)=> {
+    res.cookie('token', '').json('ok')
+})
+
+app.get('/blogs', async (req, res) => {
+    try {
+        const blogs = await Blog.find()
+            .populate('author', 'username')
+            .sort({createdAt : -1})
+            .limit(20);
+            res.json(blogs);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
 app.get('/profile', (req, res) => {
     const { token } = req.cookies;
 
@@ -93,10 +111,6 @@ app.get('/profile', (req, res) => {
     });
 });
 
-
-app.post('/logout', (req, res)=> {
-    res.cookie('token', '').json('ok')
-})
 
 app.post('/new_blog', uploadMiddleware.single('file'), async (req, res) => {
     const {originalname, path} = req.file;
@@ -139,19 +153,6 @@ app.post('/new_blog', uploadMiddleware.single('file'), async (req, res) => {
     });
 });
 
-app.get('/blogs', async (req, res) => {
-    try {
-        const blogs = await Blog.find()
-            .populate('author', 'username')
-            .sort({createdAt : -1})
-            .limit(20)
-        ;res.json(blogs);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-});
-
 app.get('/blogs/:id', async (req, res) => {
     const { id } = req.params;
     const Details = await Blog.findById(id).populate('author', ["username"]);
@@ -162,33 +163,87 @@ app.get('/blogs/:id', async (req, res) => {
     res.json(Details);
 });
 
-app.put('/blogs',uploadMiddleware.single('file'), (req,res) => {
-    const newPath = null;
-    if(req.file) {
-        const {originalname, path} = req.file;
-        const part = originalname.split(".");
-        const ext = part[part.length -1];
-        const newPath = path+'.'+ext;
-        fs.renameSync(path, newPath);
-    } 
+app.put('/blogs/:id', uploadMiddleware.single('file'), async (req, res) => {
+    const { id } = req.params;
+    const newPath = req.file ? req.file.path : null;
 
-    const {token} = req.cookies;
+    const { token } = req.cookies;
 
-    jwt.verify(token, secret , {} , async (err, info) => {
-        if(err) throw err;
-        const {id, title, content, snippet, file} = req.body;
-        const blog = await Blog.findById(id); 
-        const isAuthor = JSON.stringify(blog.author) === JSON.stringify(info.id);
-        if(!isAuthor) {
-            res.status(400)
-            throw 'You are not the author'
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) {
+            res.status(401).json({ message: 'Invalid JWT' });
+            return;
         }
 
-        await blog.updateOne({
-            title,
-            content,
-            snippet,
-            file: newPath ? newPath: blog.file
-        })
-    })
-})
+        try {
+            const { title, content, snippet } = req.body;
+            const blog = await Blog.findById(id);
+
+            if (!blog) {
+                res.status(404).json({ message: 'Blog not found' });
+                return;
+            }
+
+            const isAuthor = blog.author.toString() === info.id;
+
+            if (!isAuthor) {
+                res.status(403).json({ message: 'You are not the author' });
+                return;
+            }
+
+            blog.title = title;
+            blog.content = content;
+            blog.snippet = snippet;
+
+            if (newPath) {
+                blog.file = newPath;
+            }
+
+            const updatedBlog = await blog.save();
+            res.json(updatedBlog);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
+    });
+});
+
+app.delete('/blogs/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const blog = await Blog.findById(id);
+
+        if (!blog) {
+            res.status(404).json({ message: 'Blog not found' });
+            return;
+        }
+
+        const { token } = req.cookies;
+
+        jwt.verify(token, secret, {}, async (err, info) => {
+            if (err) {
+                res.status(401).json({ message: 'Invalid JWT' });
+                return;
+            }
+
+            const isAuthor = blog.author.toString() === info.id;
+
+            if (!isAuthor) {
+                res.status(403).json({ message: 'You are not the author' });
+                return;
+            }
+
+            // Remove the associated file (if it exists)
+            if (blog.file) {
+                fs.unlinkSync(blog.file);
+            }
+
+            await Blog.findByIdAndDelete(id);
+            res.json({ message: 'Blog deleted successfully' });
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
