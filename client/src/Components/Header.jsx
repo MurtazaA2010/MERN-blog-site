20d538142a1ca4a3fa72072b18b2fc352606c489
Murtaza Abdullah <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-21 22:38:17 +0600

    Update Header.jsx

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -4,6 +4,7 @@ import { UserContext } from '../UserContext';
 
 const Header = () => {
   const {setUserInfo, userInfo} = useContext(UserContext);
+  
   useEffect(() => {
     fetch('http://localhost:4000/profile', {
       credentials: 'include',
