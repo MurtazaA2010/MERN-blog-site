b8ac25bc5b52785be89caf353d2270c7f16a305c
Murtaza Abdullah <140527812+MurtazaA2010@users.noreply.github.com>
2024-01-26 22:47:08 +0600

    Update Blog.jsx

--- client/src/Components/Blog.jsx
+++ client/src/Components/Blog.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import '../blog.css'
+import '../CSS/blog.css'
 import {format, formatISO9075} from 'date-fns';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
@@ -23,12 +23,12 @@ const Blog = () => {
             console.error('Fetch error:', error);
         });
     }, []);
-
+    
+    
     return ( 
         <div className="blogs">
             {blogs.length > 0 && blogs.map((blog) => (
                 <div className="blog-each">
-                    
                         <div className="blog-img">
                             <Link to={`/blogs/${blog._id}`}>
                                 <img src={'http://localhost:4000/'+blog.file} alt="Image not available cureently" />
@@ -36,30 +36,27 @@ const Blog = () => {
                         </div>
                         <div className="blog-txt">
                             <div className="blog-title">
-                                <Link to={`/blogs/${blog._id}`}>                                
-                                    <h3>{blog.title}</h3>
-                                </Link>
+                                    <h3 className='title'>{blog.title}</h3>
                             </div>
                             <div className="blog-author">
-                                <Link to={`/blogs/${blog._id}`}>
-                                    <p>Author : {blog.author.username}</p>
-                                </Link>
+                                    <p>by @{blog.author.username}</p>
                             </div>
                             <div className="blog-time">
-                                <Link to={`/blogs/${blog._id}`}>
-                                    <time>{format(new Date(blog.createdAt), 'MMM d, yyyy  h:mm:ss aa')}</time>
-                                </Link>
+                                
+                                    <time>On {format(new Date(blog.createdAt), 'MMM d, yyyy')}</time>
                             </div>
                             <div className="blog-details">
-                                <Link to={`/blogs/${blog._id}`}>
                                     <p>{blog.snippet}</p>
-                                </Link>
+                            </div>
+                            <div className="button">
+                                <button> <Link to={`/blogs/${blog._id}`}>Read More</Link></button>
                             </div>
                         </div>
                 </div>
-            )
-        )}
-        </div>   
+            ))}
+        </div>
+        
      );
 }
+ 
 export default Blog;
